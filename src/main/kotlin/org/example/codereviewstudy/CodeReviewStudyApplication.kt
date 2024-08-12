d6f394315ac978bb776f179b6997d69cae7c3fb2
yongckim <zxcv9203@naver.com>
2024-08-12 22:26:51 +0900

    :tada: 프로젝트 초기화

---
+++ src/main/kotlin/org/example/codereviewstudy/CodeReviewStudyApplication.kt
@@ -0,0 +1,11 @@
+package org.example.codereviewstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodeReviewStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<CodeReviewStudyApplication>(*args)
+}
