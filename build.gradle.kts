c958fa512fa9b0f3697db274ef80b17f253ee87a
yongckim <zxcv9203@naver.com>
2024-08-19 22:38:26 +0900

    :heavy_plus_sign: config : 테스트 관련 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,14 @@ plugins {
     kotlin("plugin.spring") version "1.9.24"
     id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.6"
+    id("org.jetbrains.kotlin.plugin.jpa") version "1.9.24"
 }
 
 group = "org.example"
 version = "0.0.1-SNAPSHOT"
+val kotestVersion = "5.9.1"
+val kotestSpringExtensionVersion = "1.3.0"
+val mockkVersion = "1.13.12"
 
 java {
     toolchain {
@@ -26,8 +30,24 @@ dependencies {
     implementation("org.flywaydb:flyway-core")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     runtimeOnly("com.h2database:h2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // kotest
+    testImplementation ("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation ("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:$kotestSpringExtensionVersion")
+
+    // mockk
+    testImplementation("io.mockk:mockk:$mockkVersion")
+
+    // passwordEncoder
+    implementation("org.springframework.security:spring-security-crypto")
 }
 
 kotlin {
