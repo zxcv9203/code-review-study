57ca83e2f8cb23b82dd7fa4f61cd39b62eb650c6
yongckim <zxcv9203@naver.com>
2024-08-19 21:06:08 +0900

    :sparkles: feat : 생성, 수정 시간을 자동으로 넣어주도록 Auditing 추상 클래스 구현

---
+++ src/main/kotlin/org/example/codereviewstudy/infrastructure/audit/config/AuditConfig.kt
@@ -0,0 +1,8 @@
+package org.example.codereviewstudy.infrastructure.audit.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class AuditConfig
