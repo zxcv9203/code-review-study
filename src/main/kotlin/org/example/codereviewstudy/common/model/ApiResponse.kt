2198bc7adb8ac79b0af7d79d74c9d10b259d0cf2
yongckim <zxcv9203@naver.com>
2024-08-26 19:54:13 +0900

    :recycle: refactor : status는 HTTP Status를 보내는 용도로만 사용하고 있어 Int 타입으로 제한하도록 변경

--- src/main/kotlin/org/example/codereviewstudy/common/model/ApiResponse.kt
+++ src/main/kotlin/org/example/codereviewstudy/common/model/ApiResponse.kt
@@ -3,23 +3,23 @@ package org.example.codereviewstudy.common.model
 import org.springframework.http.HttpStatus
 
 data class ApiResponse<T>(
-    val status: String,
+    val status: Int,
     val message: String,
     val data: T,
 ) {
     companion object {
         fun <T> success(status: HttpStatus, message: String, data: T): ApiResponse<T> {
-            return ApiResponse(status.value().toString(), message, data)
+            return ApiResponse(status.value(), message, data)
         }
         fun success(status: HttpStatus, message: String): ApiResponse<Unit> {
-            return ApiResponse(status.value().toString(), message, Unit)
+            return ApiResponse(status.value(), message, Unit)
         }
 
         fun error(status: HttpStatus, message: String): ApiResponse<Unit> {
-            return ApiResponse(status.value().toString(), message, Unit)
+            return ApiResponse(status.value(), message, Unit)
         }
         fun <T>error(status: HttpStatus, message: String, data: T): ApiResponse<T> {
-            return ApiResponse(status.value().toString(), message, data)
+            return ApiResponse(status.value(), message, data)
         }
     }
 }
