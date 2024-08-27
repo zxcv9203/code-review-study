d6c87b168efef9c13f5b43991baf4807d5d8e392
yongckim <zxcv9203@naver.com>
2024-08-27 20:06:41 +0900

    :sparkles: feat : 아이디로 유저 정보 조회 메서드 추가

--- src/main/kotlin/org/example/codereviewstudy/infrastructure/persistence/user/UserRepository.kt
+++ src/main/kotlin/org/example/codereviewstudy/infrastructure/persistence/user/UserRepository.kt
@@ -5,6 +5,7 @@ import org.example.codereviewstudy.domain.user.model.toJpaEntity
 import org.example.codereviewstudy.domain.user.port.UserQueryPort
 import org.example.codereviewstudy.domain.user.port.UserRegistrationPort
 import org.example.codereviewstudy.domain.user.port.UserValidationPort
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -23,6 +24,10 @@ class UserRepository(
     override fun findByUsername(username: String): User? {
         return jpaUserRepository.findByUsername(username)
             ?.toUser()
+    }
 
+    override fun findById(id: Long): User? {
+        return jpaUserRepository.findByIdOrNull(id)
+            ?.toUser()
     }
 }
