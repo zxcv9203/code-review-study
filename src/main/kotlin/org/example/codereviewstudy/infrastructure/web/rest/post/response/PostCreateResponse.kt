3e0020d10b0cd3f6a2c3d7e86df63fe56da70cf5
yongckim <zxcv9203@naver.com>
2024-08-28 21:04:19 +0900

    :recycle: refactor : 게시글 생성시 id도 반환하도록 변경

--- src/main/kotlin/org/example/codereviewstudy/infrastructure/web/rest/post/response/PostCreateResponse.kt
+++ src/main/kotlin/org/example/codereviewstudy/infrastructure/web/rest/post/response/PostCreateResponse.kt
@@ -3,6 +3,7 @@ package org.example.codereviewstudy.infrastructure.web.rest.post.response
 import java.time.LocalDateTime
 
 data class PostCreateResponse(
+    val id: Long,
     val title: String,
     val author: String,
     val content: String,
